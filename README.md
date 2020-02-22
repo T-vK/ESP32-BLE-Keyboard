e4397695be96b8c3282117fa2cc1ffaffc8d68a8
Dean Blackketter <dean@blackketter.com>
2020-02-22 15:37:59 -0800

    Cleanup example a little

--- README.md
+++ README.md
@@ -21,6 +21,53 @@ You might also be interested in:
 
 ## Example
 
+```
+#include <BleComboKeyboard.h>
+#include <BleComboMouse.h>
+
+BleComboKeyboard keyboard;
+BleComboMouse mouse(&keyboard);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting work!");
+  keyboard.begin();
+  mouse.begin();
+}
+
+void loop() {
+  if(keyboard.isConnected()) {
+    Serial.println("Sending 'Hello world'");
+    keyboard.println("Hello World");
+
+    Serial.println("Sending Enter key...");
+    keyboard.write(KEY_RETURN);
+
+    Serial.println("Sending Play/Pause media key...");
+    keyboard.write(KEY_MEDIA_PLAY_PAUSE);
+
+    Serial.println("Sending Ctrl+Alt+Delete...");
+    keyboard.press(KEY_LEFT_CTRL);
+    keyboard.press(KEY_LEFT_ALT);
+    keyboard.press(KEY_DELETE);
+    delay(100);
+    keyboard.releaseAll();
+
+    Serial.println("Move mouse pointer up");
+    mouse.move(0,-10);
+    
+    Serial.println("Scroll Down");
+    mouse.move(0,0,-1);
+
+    Serial.println("Left click");
+    mouse.click(MOUSE_LEFT);
+  }
+  
+  Serial.println("Waiting 2 seconds...");
+  delay(2000);
+}
+
+```
 ## Credits
 
 This is fork of @T-kV's excellent [ESP32-BLE-Mouse](https://github.com/T-vK/ESP32-BLE-Mouse)
--- examples/KeyboardMouseExample/KeyboardMouseExample.ino
+++ examples/KeyboardMouseExample/KeyboardMouseExample.ino
@@ -1,7 +1,7 @@
 /**
  * This example turns the ESP32 into a Bluetooth LE keyboard and mouse that writes 
- * the words, presses Enter, presses a media key and then Ctrl+Alt+Delete,
- * then moves the mouse and clicks it.
+ * some words, presses Enter, presses a media key and then Ctrl+Alt+Delete,
+ * then moves and the scrolls the mouse and clicks it.
  */
  
 #include <BleComboKeyboard.h>
@@ -18,16 +18,16 @@ void setup() {
 }
 
 void loop() {
-  static int i = 0;
   if(keyboard.isConnected()) {
-    Serial.println("Sending 'Hi Mom'...");
-    keyboard.printf("Hi Mom =1234567890-\\;',./`[]  +!@#$%%^&*()_|:\"<>?~{} %d", i++);
+    Serial.println("Sending 'Hello world'");
+    keyboard.println("Hello World");
 
+    delay(1000);
     Serial.println("Sending Enter key...");
     keyboard.write(KEY_RETURN);
 
-/*
     delay(1000);
+  
     Serial.println("Sending Play/Pause media key...");
     keyboard.write(KEY_MEDIA_PLAY_PAUSE);
 
@@ -39,7 +39,6 @@ void loop() {
     keyboard.press(KEY_DELETE);
     delay(100);
     keyboard.releaseAll();
-*/
 
     unsigned long startTime;
 
@@ -69,9 +68,41 @@ void loop() {
       mouse.move(1,0);
       delay(5);
     }
+    
+    Serial.println("Scroll Down");
+    mouse.move(0,0,-1);
+
+    Serial.println("Left click");
+    mouse.click(MOUSE_LEFT);
+    delay(500);
+
+    Serial.println("Right click");
+    mouse.click(MOUSE_RIGHT);
+    delay(500);
+
+    Serial.println("Scroll wheel click");
+    mouse.click(MOUSE_MIDDLE);
+    delay(500);
+
+    Serial.println("Back button click");
+    mouse.click(MOUSE_BACK);
+    delay(500);
+
+    Serial.println("Forward button click");
+    mouse.click(MOUSE_FORWARD);
+    delay(500);
+
+    Serial.println("Click left+right mouse button at the same time");
+    mouse.click(MOUSE_LEFT | MOUSE_RIGHT);
+    delay(500);
+
+    Serial.println("Click left+right mouse button and scroll wheel at the same time");
+    mouse.click(MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE);
+    delay(500);
+
 
   }
-
+  
   Serial.println("Waiting 2 seconds...");
   delay(2000);
 }
